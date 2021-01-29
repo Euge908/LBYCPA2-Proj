1d70fe0aaa658115bf7c0b843091ce8460251d0f
davewg1 <78000662+davewg1@users.noreply.github.com>
2021-01-29 16:10:39 +0800

    added admin view ui

--- src/sample/Subject.java
+++ src/sample/Subject.java
@@ -1,124 +1,35 @@
-package sample;
-
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Subject {
-    public String name;
-    public int subjectUnit;
-    public String time;
+    private String code;
+    private String name;
+    private int units;
 
-    public LinkedList<Student> studentList = new LinkedList<>();
+    private List<String> timeSlots = new ArrayList<>();
 
-
-
-    //Constructor
-    Subject(String name, String time) {
+    public Subject(String name,String code,String[] slots,int units ) {
+        this.code = code;
         this.name = name;
-        this.time = time;
-        getSubjectUnit();
+        Collections.addAll(timeSlots, slots);
+        this.units = units;
+    }
 
+    public String getCode() {
+        return this.code;
     }
 
     public String getName(){
-        return name;
+        return this.name;
     }
 
-
-    public String getTime(){
-        return time;
+    public int getUnits(){
+        return this.units;
     }
 
-    public int getSubjectUnit() {
-
-
-        if (name.toLowerCase().contains("ge")) {
-            this.subjectUnit =  3;
-        } else if (name.toLowerCase().contains("eng")) {
-            this.subjectUnit =  3;
-        } else if (name.toLowerCase().contains("fnd")) {
-            this.subjectUnit =  3;
-        } else if (name.toLowerCase().contains("pro")) {
-            this.subjectUnit =  2;
-
-        }else if (name.toLowerCase().contains("diff")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("dat")) {
-            this.subjectUnit =  1;
-
-        }else if (name.toLowerCase().contains("dis")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("fund")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("num")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("sof")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("dig")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("log")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("oper")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("mic")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("re")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("fdc")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("com")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("emb")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("lca")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("mx")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("cpe")) {
-            this.subjectUnit =  2;
-
-        }else if (name.toLowerCase().contains("och")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("lce")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("emer")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("con")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("ths")) {
-            this.subjectUnit =  1;
-
-        }else if (name.toLowerCase().contains("ecn")) {
-            this.subjectUnit =  3;
-
-        }else if (name.toLowerCase().contains("lby")) {
-            this.subjectUnit =  1;
-        }else if (name.toLowerCase().contains("lby")) {
-            this.subjectUnit =  1;
-        }
-        
-        this.subjectUnit = 0;
-        return subjectUnit;
+    public List getTimeSlots(){
+        return this.timeSlots;
     }
-
-
 
 }
