5dd86d5b8e2c7758e4e8157d4652abf6a4eb30d5
davewg1 <78000662+davewg1@users.noreply.github.com>
2021-01-29 19:41:49 +0800

    edit admin view

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("studentView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("adminView.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1080, 720));
         primaryStage.show();
