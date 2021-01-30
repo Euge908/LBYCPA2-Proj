0fbfd2026d83b474d80d22bbe0aa79b75e0a7735
Felix Montanez <felixgabrielmontanez@gmail.com>
2021-01-30 21:19:39 +0800

    added timeslot hashmap

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,27 @@
 package sample;
 
+import java.util.HashMap;
+
 public class Controller {
+
+    public void initialize() {
+
+        initializeTimeSlot();
+
+    }
+
+    private void initializeTimeSlot() {
+        HashMap<String, String[]> timeSlot = new HashMap<String, String[]>();
+        timeSlot.put("caleng2", new String[]{"14:15-17:45,TH", "15:15-17:45,MW"});
+        timeSlot.put("engchem", new String[]{"07:30-09:00,TH", "09:15-10:45,TH", "07:30-09:00,MW"});
+        timeSlot.put("lbych1a", new String[]{"09:15-12:15,T", "14:30-17:30,W", "14:30-17:30,T"});
+        timeSlot.put("lclsone", new String[]{"07:30-9:30,M", "10:30-12:00,F", "16:30-18:30,W"});
+        timeSlot.put("geethic", new String[]{"14:30-16:00,TH", "12:45-14:15,MW", "07:30-09:00,MW"});
+        timeSlot.put("datsral", new String[]{"14:30-15:30,M", "16:15-15:15,T", "16:15-17:15,M"});
+        timeSlot.put("discrmt", new String[]{"12:45-14:15,MW", "11:00-12:30,TH", "09:15-10:45,TH"});
+        timeSlot.put("fndckt", new String[]{"16:15-17:45,MW", "11:00-12:30,TH", "14:30-16:00,MW"});
+        timeSlot.put("lbycpa2", new String[]{"09:15-12:15,W", "09:15-12:15,M", "14:30-17:30,H"});
+        timeSlot.put("lbyec2m", new String[]{"09:15-12:15,M", "09:15-12:15,W", "14:30-17:30,T"});
+    }
+
 }
