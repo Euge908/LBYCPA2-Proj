2253300945a08daad5b9665a5cea36290c572ea8
Felix Montanez <felixgabrielmontanez@gmail.com>
2021-02-01 01:13:22 +0800

    made the transition for login to student view

--- src/sample/loginController.java
+++ src/sample/loginController.java
@@ -1,13 +1,20 @@
 package sample;
 
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class loginController {
 
@@ -24,19 +31,24 @@ public class loginController {
             @Override
             public void handle(KeyEvent keyEvent) {
                 if (keyEvent.getCode() == KeyCode.ENTER)  {
-                    signIn();
+                    try {
+                        signIn();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
         });
     }
 
-    public void Login(MouseEvent mouseEvent) {
+    public void Login(MouseEvent mouseEvent) throws IOException {
         signIn();
     }
 
-    public void signIn(){
+    public void signIn() throws IOException {
         if(username.getText().equals("user") && password.getText().equals("pass")){
             status.setText("Success");
+
             /*if(T instance of student){
                     load student view fxml (pass matched student)
               } else{
@@ -44,6 +56,25 @@ public class loginController {
                     pass matched admin
                 }
              */
+
+
+            //open student view
+            try {
+
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../sample/studentView.fxml"));
+                Parent root1 = (Parent) fxmlLoader.load();
+                Stage stage = new Stage();
+                stage.setTitle("Student View");
+                stage.setScene(new Scene(root1));
+                stage.show();
+                //close login
+                Stage thisStage = (Stage) status.getScene().getWindow();
+                thisStage.close();
+            }catch(Exception e){
+                System.out.println("Cant load window");
+            }
+
+
 
         }
         else{
