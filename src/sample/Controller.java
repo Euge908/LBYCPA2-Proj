6132aa080aa5c4a7e4cf82aad9c7322747250764
eugene reyes <eugenecedricngoreyes@gmail.com>
2021-02-01 11:00:42 +0800

    Fixed Merging and stuff

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -38,7 +38,9 @@ public class Controller {
 
     private HashMap<String, String[]> timeSlot = new HashMap<>(); //replace this na lang
     private LinkedList<Subject> subjects = new LinkedList<>();
-    private Student currentStudent = new Student("Jon Doe", "11917113", 22);
+
+    //dummy student
+    private Student currentStudent = new Student("felix", "felix@dlsu.edu.ph","pass1","119106606", 18);
 
     private ObservableList<Subject> data = FXCollections.observableArrayList();
 
@@ -67,28 +69,12 @@ public class Controller {
 
     }
 
-    private void initializeTimeSlot() {
-        timeSlot.put("caleng2", new String[]{"14:15-17:45,TH", "15:15-17:45,MW"});
-        timeSlot.put("engchem", new String[]{"07:30-09:00,TH", "09:15-10:45,TH", "07:30-09:00,MW"});
-        timeSlot.put("lbych1a", new String[]{"09:15-12:15,T", "14:30-17:30,W", "14:30-17:30,T"});
-        timeSlot.put("lclsone", new String[]{"07:30-9:30,M", "10:30-12:00,F", "16:30-18:30,W"});
-        timeSlot.put("geethic", new String[]{"14:30-16:00,TH", "12:45-14:15,MW", "07:30-09:00,MW"});
-        timeSlot.put("datsral", new String[]{"14:30-15:30,M", "16:15-15:15,T", "16:15-17:15,M"});
-        timeSlot.put("discrmt", new String[]{"12:45-14:15,MW", "11:00-12:30,TH", "09:15-10:45,TH"});
-        timeSlot.put("fndckt", new String[]{"16:15-17:45,MW", "11:00-12:30,TH", "14:30-16:00,MW"});
-        timeSlot.put("lbycpa2", new String[]{"09:15-12:15,W", "09:15-12:15,M", "14:30-17:30,H"});
-        timeSlot.put("lbyec2m", new String[]{"09:15-12:15,M", "09:15-12:15,W", "14:30-17:30,T"});
-        ///new changes
-
-
-    }
-
     public void addCourse(){
         String course = courseTextField.getText();
 
         //add condition to check the time to avoid bugs
         //^ above will create erronous time if smart ass uses it up
-
+        //also simplify the table 
 
         if(timeSlot.containsKey(course)){
             data.add(new Subject(course, selectedTime + "," +selectedDay));
@@ -131,21 +117,6 @@ public class Controller {
                 return;
             }
         }
-
-    }
-
-    public void enrollCourse(){
-
-    }
-
-
-
-
-    HashMap<String, String[]> timeSlot= new HashMap<String, String[]>();;
-
-    public void initialize() {
-
-        initializeTimeSlot();
 
     }
 
