f0861c207734ccc733fb3b4b57db2014a37611cf
davewg1 <78000662+davewg1@users.noreply.github.com>
2021-02-03 00:00:43 +0800

    working

--- src/student/studentController.java
+++ src/student/studentController.java
@@ -165,7 +165,7 @@ public class studentController {
         currentUserNameLabel.setText(currentStudent.name);
         currentUserEmailLabel.setText(currentStudent.email);
 
-        Image temp = new Image("file:assets/" + currentStudent.pic);
+        Image temp = new Image("file:assets/pictures/" + currentStudent.pic);
 
         //if there is no error
         if(!temp.isError()) {
@@ -176,7 +176,8 @@ public class studentController {
 
         //notify if error
         else{
-            display("File might be:\nmissing/invalid/unsupported/not in assets folder");
+            System.out.println("w");
+            display("File might be:\nmissing/invalid/unsupported/not in assets/pictures/ folder");
         }
 
         welcomeTxt.setText("Welcome " + currentStudent.name + " to your dashboard!");
